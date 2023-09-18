454a671e02753b722b2f8048b6a7b9db5dd58c19
Parmod <panchalpritam105@gmail.com>
2023-09-19 01:09:27 +0530

    pp

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(props){
         <>
         <nav className={`navbar navbar-expand-lg bg-${props.Mode} navbar-${props.Mode}`}>
   <div className={`container-fluid`}>
-    <a className={`navbar-brand  mx-5 `}><strong>TextUtils</strong></a>
+    <a className={`navbar-brand  mx-5 `}><strong>TextUtillls</strong></a>
     <div className="form-check form-switch  ">
   <input className="form-check-input" onClick={props.togelMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
   <label className="form-check-label me-5" htmlFor="flexSwitchCheckDefault">{props.darkLight}</label>
